b340c1e6f3c99f6ffbb6f03ffe3c021596c7a105
gustter <gretter@hotmail.com>
2023-05-10 17:44:47 -0300

    Update demo and templates.

--- demo/www/custom/custom.js
+++ demo/www/custom/custom.js
@@ -3,14 +3,17 @@
 function deviceReady() {
 	
 	try {
+
 		//Example when Internet connection is needed but not mandatory
 		//Ejemplo de cuando se necesita conexióna a Internet pero no es obligatoria.
+		/*
 		if (!mui.connectionAvailable()){
 			if ('plugins' in window && 'toast' in window.plugins)
 				mui.toast('We recommend you connect your device to the Internet');
 			else
 				mui.alert('We recommend you connect your device to the Internet');
 		}
+		*/
 		
 		//Install events, clicks, resize, online/offline, etc. 
 		installEvents();
@@ -31,6 +34,7 @@ function deviceReady() {
  */
 function installEvents() {
 	
+  //ATENCIÓN, ESTA TÉCNICA, deprecated, es solo válida para eventos iniciales
 	mui.util.installEvents([
 		//It's a good idea to consider what happens when the device is switched on and off the internet.
 		//Es buena idea considerar que pasa cuando el dispositivo se conecta y desconecta a Internet.
